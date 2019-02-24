3d48e331b0fe14e34e4791cb0906186f89c0b6c7
Tabassum Syed (HCL America Inc) <v-tasyed@microsoft.com>
2019-02-24 22:25:35 +0530

    Add project files.

---
+++ test123/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    for (int i = 1; i <=25; i++)
+            //    {
+            //        if (i % 3 ==0)
+            //        {
+            //            continue;
+            //        }
+            //        Console.WriteLine(i);
+            //    }
+            //    Console.ReadLine();
+
+            //}
+            //int r;
+            //double a;
+            //const double pi = 3.14;
+            //Console.Write("Enter the radius: ");
+            //r = Convert.ToInt32(Console.ReadLine());
+            //a = pi * r * r;
+            //Console.WriteLine("Area of circle is = " + a);
+            //Console.ReadLine();
+
+            
+            Console.WriteLine("Enter a number: ");
+            //Console.ReadLine();
+            string NumberInStringFormat = Console.ReadLine();
+            int NumberInIntFormat = Convert.ToInt32(NumberInStringFormat);
+            if (NumberInIntFormat % 2 == 0)
+            {
+                Console.Write("The Number entered is Even");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("The Number entered is odd");
+                Console.Read();
+            }
+        }
+    }
+}
+
+
